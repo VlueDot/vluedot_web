13cc06227f99013efaae7d79c22d455f138f0938
Vincerr <rvin.rdgz@gmail.com>
2024-11-22 13:45:47 -0500

    adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import {  Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import $ from 'jquery'
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeVlueioComponent } from './components/home-vlueio/home-vlueio.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ],
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.scss'
-  template: '<router-outlet></router-outlet>'
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, HomeVlueioComponent ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+  // template: '<router-outlet></router-outlet>'
 })
 export class AppComponent  {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
+// import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HomeVlueioComponent } from '../../components/home-vlueio/home-vlueio.component';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from '../../components/footer/footer.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HomeVlueioComponent, CommonModule, FooterComponent],
+  imports: [ HomeVlueioComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
